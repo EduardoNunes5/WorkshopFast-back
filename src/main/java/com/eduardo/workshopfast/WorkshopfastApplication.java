44e570ae740fe08bd49173ebb246a0e95bbd30cf
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2025-10-30 10:30:06 -0300

    initial commit

---
+++ src/main/java/com/eduardo/workshopfast/WorkshopfastApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.workshopfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopfastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopfastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/workshopfast/WorkshopfastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.workshopfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopfastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
