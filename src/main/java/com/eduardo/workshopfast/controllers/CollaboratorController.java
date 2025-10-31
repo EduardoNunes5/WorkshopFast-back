3488348fff84729fed8ff04bd7816a04b91edd95
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2025-10-30 21:58:47 -0300

    add endpoint to register new collaborator

--- src/main/java/com/eduardo/workshopfast/controllers/CollaboratorController.java
+++ src/main/java/com/eduardo/workshopfast/controllers/CollaboratorController.java
@@ -1,9 +1,25 @@
 package com.eduardo.workshopfast.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eduardo.workshopfast.dto.collaborator.SaveCollaboratorRequestDto;
+import com.eduardo.workshopfast.dto.collaborator.SaveCollaboratorResponseDto;
+import com.eduardo.workshopfast.services.CollaboratorService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/colaboradores")
 public class CollaboratorController {
+
+    private final CollaboratorService service;
+
+    public CollaboratorController(CollaboratorService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public SaveCollaboratorResponseDto create(@RequestBody @Valid SaveCollaboratorRequestDto collaboratorRequestDto) {
+        return service.create(collaboratorRequestDto);
+    }
 }
--- src/main/java/com/eduardo/workshopfast/services/impl/CollaboratorServiceImpl.java
+++ src/main/java/com/eduardo/workshopfast/services/impl/CollaboratorServiceImpl.java
@@ -1,8 +1,27 @@
 package com.eduardo.workshopfast.services.impl;
 
+import com.eduardo.workshopfast.dto.collaborator.SaveCollaboratorRequestDto;
+import com.eduardo.workshopfast.dto.collaborator.SaveCollaboratorResponseDto;
+import com.eduardo.workshopfast.entities.Collaborator;
+import com.eduardo.workshopfast.repositories.CollaboratorRepository;
 import com.eduardo.workshopfast.services.CollaboratorService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 class CollaboratorServiceImpl implements CollaboratorService {
+
+    private final CollaboratorRepository repository;
+
+    CollaboratorServiceImpl(CollaboratorRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    @Transactional
+    public SaveCollaboratorResponseDto create(SaveCollaboratorRequestDto collaboratorRequestDto) {
+        Collaborator savedCollaborator = new Collaborator(collaboratorRequestDto.name());
+        savedCollaborator = repository.save(savedCollaborator);
+        return new SaveCollaboratorResponseDto(savedCollaborator);
+    }
 }
