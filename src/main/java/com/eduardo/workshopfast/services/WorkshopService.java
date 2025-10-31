e2c9a4879447b5f7f188e824f5ee0f31ae2214d7
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2025-10-31 20:09:24 -0300

    feature: add service and query to retrieve workshops with collaborators by filters

--- src/main/java/com/eduardo/workshopfast/services/WorkshopService.java
+++ src/main/java/com/eduardo/workshopfast/services/WorkshopService.java
@@ -2,11 +2,16 @@ package com.eduardo.workshopfast.services;
 
 import com.eduardo.workshopfast.dto.workshop.SaveWorkshopRequestDto;
 import com.eduardo.workshopfast.dto.workshop.WorkshopDto;
+import com.eduardo.workshopfast.dto.workshop.WorkshopFilterDto;
 import com.eduardo.workshopfast.entities.Workshop;
+
+import java.util.List;
 
 public interface WorkshopService {
 
     WorkshopDto create(SaveWorkshopRequestDto requestDto);
 
     Workshop getReferenceById(Long id);
+
+    List<Workshop> findByFilters(WorkshopFilterDto filter);
 }
