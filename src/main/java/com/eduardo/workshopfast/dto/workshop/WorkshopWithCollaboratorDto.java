e0827932138e6dd0fa6f36713cc9fe8b922d2c4c
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2025-11-02 21:15:52 -0300

    feature: add dto with data related to collaborator, workshop and workshop attendance

--- src/main/java/com/eduardo/workshopfast/dto/workshop/WorkshopWithCollaboratorDto.java
+++ src/main/java/com/eduardo/workshopfast/dto/workshop/WorkshopWithCollaboratorDto.java
@@ -1,15 +1,20 @@
 package com.eduardo.workshopfast.dto.workshop;
 
-import com.eduardo.workshopfast.dto.collaborator.CollaboratorIdNameDto;
+import com.eduardo.workshopfast.dto.collaborator.CollaboratorWorkshopAttendanceDto;
 import com.eduardo.workshopfast.entities.Workshop;
+import com.eduardo.workshopfast.entities.WorkshopAttendance;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record WorkshopWithCollaboratorDto(
         Long id,
         String name,
         String description,
-        List<CollaboratorIdNameDto> collaborators
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+        LocalDateTime realizationDate,
+        List<CollaboratorWorkshopAttendanceDto> collaborators
 ) {
 
     public WorkshopWithCollaboratorDto(Workshop entity) {
@@ -17,7 +22,14 @@ public record WorkshopWithCollaboratorDto(
                 entity.getId(),
                 entity.getName(),
                 entity.getDescription(),
-                entity.getWorkshopAttendance() != null ? entity.getWorkshopAttendance().getCollaborators().stream().map(CollaboratorIdNameDto::new).toList() : List.of()
+                entity.getRealizationDate(),
+                entity.getWorkshopAttendance() != null ? workshopAttendanceToDto(entity.getWorkshopAttendance()) : List.of()
         );
+    }
+
+    private static List<CollaboratorWorkshopAttendanceDto> workshopAttendanceToDto(WorkshopAttendance workshopAttendance) {
+        return workshopAttendance.getCollaborators().stream()
+                .map((collaborator) -> new CollaboratorWorkshopAttendanceDto(workshopAttendance, collaborator))
+                .toList();
     }
 }
